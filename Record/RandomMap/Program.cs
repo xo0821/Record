4334454170f02fb782b1914ae8c0a14c4376255c
msxo <xo0821ms@gmail.com>
2021-10-25 11:51:30 +0800

    打印及產生陣列內容
    產生陣列內容註記:
    1.CteatArray_SquareDivision
    創建數字順序宮格
    (
    1-9就是
    123
    456
    789)
    
    2.CteatArray:根據輸入字串跟對應維度切割成宮格(預留讀入檔案轉成字串後，可以轉陣列)
    ###尚未完全###還需要判定字串長度不符合的情形，或是更完整點直接判斷長度符合就自動轉宮格不符合直接丟錯誤。
    (
    "000010010"會變成
    000
    010
    010
    )

---
+++ Record/RandomMap/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
+using System.Security;
+namespace RandomMap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayOperate.DrawArry(ArrayOperate.CteatArray(4, 4,"0000001000101110"));
+            Console.ReadLine();
+        }
+    }
+    
+        public static partial class ArrayOperate
+    {
+        public  static bool fill()
+        {
+            Vector2 v = new Vector2();
+
+            return true;
+        }
+        
+        public static Boolean inArea(int[,]image, int x, int y){
+            return x>=0&&x<image.Length&&y>=0&&y<image.GetLength(0);
+        }
+        public static void DrawArry(int[,] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i,j]);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int[,] CteatArray_SquareDivision(int x,int y)//創建數字順序的九宮格
+        {
+            int s =0;
+            int[,] a = new int[x,y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    s++;
+                    a[i,j] = s;
+                }
+            }
+            return a;
+        }
+        public static int[,] CteatArray(int x,int y,string str)
+        {
+            List<int> list = StringToInt(str);
+            int s =0;
+            int[,] a = new int[x,y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    a[i,j] = list[s];
+                    s++;
+                }
+            }
+            return a;
+        }
+        static List<int> StringToInt(string s)
+        {
+            var l = GetChar(s, 1);
+            var i =new List<int>();
+            foreach (var item in l)
+            {
+                i.Add((int.Parse(item)));
+            }
+
+            return i;
+        }
+        static IEnumerable<string> GetChar(string source,int deg)
+        {
+            var words = new List<string>();
+            for (int i = 0; i < source.Length; i+=deg)
+            {
+                if (source.Length-i>=deg) words.Add(source.Substring(i,deg));
+                else
+                {
+                    words.Add(source.Substring(i,source.Length-i));  
+                }
+            }
+            return words;
+        }
+    }
+}
